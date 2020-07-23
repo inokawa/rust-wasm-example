dda00a11006578320322c7b7158db5e1dd407d2b
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-07-23 18:17:17 +0900

    Use addRow helper

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 (async () => {
   const wasm = await import("../pkg/index.js");
 
+  const addRow = (...elems) => {
+    const div = document.createElement("div");
+    elems.forEach((el) => {
+      div.appendChild(el);
+    });
+    document.body.appendChild(div);
+  };
+
   const cButton = document.createElement("button");
   cButton.appendChild(document.createTextNode("Hello from C"));
   cButton.addEventListener("click", () => {
     const num = wasm.greet_c(99);
     alert(num);
   });
-  document.body.appendChild(cButton);
+  addRow(cButton);
 })();
