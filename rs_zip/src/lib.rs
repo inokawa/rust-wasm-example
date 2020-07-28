02d2e92f8b64400b1b393a932939fcf89f969d62
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-07-29 02:46:23 +0900

    Implement archiving with gzip

--- rs_zip/src/lib.rs
+++ rs_zip/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::io::prelude::*;
+
+pub fn gzip(buffer: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut e = GzEncoder::new(Vec::new(), Compression::default());
+    e.write_all(buffer)?;
+    e.finish()
 }
